f16a1105eecc50a17d2c07eb00d96f783f2b1bf9
Srdjan <stojanovicsrdjan27@gmail.com>
2025-04-16 10:25:02 +0200

    Create ip-geomaster-public-display.php

---
+++ public/partials/ip-geomaster-public-display.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://ipgeomaster.icodes.rocks
+ * @since      1.0.0
+ *
+ * @package    Ip_Geomaster
+ * @subpackage Ip_Geomaster/public/partials
+ */
+?>
